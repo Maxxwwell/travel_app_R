95d88b534c501dbdca08eb9717969c265b258dfb
Maxxwwell <agyeimaxwell54@gmail.com>
2021-11-29 17:44:22 +0000

    navigation works

---
+++ app/screens/Home.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text} from 'react-native';
+
+const Home = () => {
+  return <Text>Home</Text>;
+};
+
+export default Home;
---
+++ app/screens/Onboarding.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text } from 'react-native';
+
+const Onboarding = () => {
+    return <Text>Onboarding</Text>;
+};
+
+export default Onboarding;
